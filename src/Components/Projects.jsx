4f3e462c4da29ce27003662d7d1ddcf5cee0aafa
Nitishkumar <nitishsbm2016@gmail.com>
2024-05-24 06:26:10 +0530

    changed

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "../Components/Projects.css"
 
 import portfolioimg from '../../public/portfolioimg.png'
-import Bookstore from '../../public/Bookstore.jpg'
+import WordtoPDF from '../../public/WordtoPDF.png'
 import fruitslicerimg from '../../public/fruitslicerimg.jpg'
 import weather from '../../public/weather.jpg'
 
@@ -17,27 +17,33 @@ export default function Projects() {
       <h1 className="text-3xl font-bold mb-5">Projects</h1>
       <div className="projectsBox ">
                 <div className="box1">
+                     
                     <div className="headingportfolio">
                     <img src={portfolioimg} alt=""  className="portfolioimg"/>
-                        <h4>Portfolio</h4>
+                        <h4 className='portfolioh4'>Portfolio</h4>
+                          <a href="https://jade-alfajores-68b0cf.netlify.app/" target='_blank' className='livelink'>Live</a>
                     </div>
                 </div>
                 <div className="box1">
                     <div className="Bookstore">
-                    <img src={Bookstore} alt="" className="Bookimg"/>
-                        <h4 className='Books'>BookStore website</h4>
+                    <img src={WordtoPDF} alt="" className="Bookimg"/>
+                        <h4 className='Books'>Converter App</h4>
+                        <a href="https://jade-alfajores-68b0cf.netlify.app/" target='_blank' className='livelink'>Live</a>
                     </div>
                 </div>
                 <div className="box1">
                     <div className="Fruits">
                     <img src={fruitslicerimg} alt="" className="Fruitsimg"/>
                         <h4 className='Fruith4'>Fruit Slicer Game</h4>
+                        <a href="https://splendid-crisp-8e6cf1.netlify.app" target='_blank' className='livelink'>Live</a>
                     </div>
+                     
                 </div>
                 <div className="box1">
                     <div className="weather">
                     <img src={weather} alt="" className="weatherimg"/>
-                        <h4  className='weatherh4' >Weather Report</h4>
+                        <h4  className='weatherh4' >Weather Widget</h4>
+                        <a href="https://thunderous-otter-bc7299.netlify.app" target='_blank' className='livelink'>Live</a>
                     </div>
                 </div>
             </div>
