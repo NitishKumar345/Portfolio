ec924f9a1d962e05eb372d9e48a07f45066428ef
Nitishkumar <nitishsbm2016@gmail.com>
2024-05-28 14:28:45 +0530

    done !!

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -21,21 +21,21 @@ export default function Projects() {
                     <div className="headingportfolio">
                     <img src={portfolioimg} alt=""  className="portfolioimg"/>
                         <h4 className='portfolioh4'>Portfolio</h4>
-                          <a href="https://jade-alfajores-68b0cf.netlify.app/" target='_blank' className='livelink'>Live</a>
+                          <a href="https://nitish-portfolio1.netlify.app/" target='_blank' className='livelink'>Live</a>
                     </div>
                 </div>
                 <div className="box1">
                     <div className="Bookstore">
                     <img src={WordtoPDF} alt="" className="Bookimg"/>
                         <h4 className='Books'>Converter App</h4>
-                        <a href="https://jade-alfajores-68b0cf.netlify.app/" target='_blank' className='livelink'>Live</a>
+                        <a href="https://converter-app.netlify.app/" target='_blank' className='livelink'>Live</a>
                     </div>
                 </div>
                 <div className="box1">
                     <div className="Fruits">
                     <img src={fruitslicerimg} alt="" className="Fruitsimg"/>
                         <h4 className='Fruith4'>Fruit Slicer Game</h4>
-                        <a href="https://splendid-crisp-8e6cf1.netlify.app" target='_blank' className='livelink'>Live</a>
+                        <a href="https://fruit-slicer-game.netlify.app/" target='_blank' className='livelink'>Live</a>
                     </div>
                      
                 </div>
@@ -43,7 +43,7 @@ export default function Projects() {
                     <div className="weather">
                     <img src={weather} alt="" className="weatherimg"/>
                         <h4  className='weatherh4' >Weather Widget</h4>
-                        <a href="https://thunderous-otter-bc7299.netlify.app" target='_blank' className='livelink'>Live</a>
+                        <a href="https://weather-widget1.netlify.app/" target='_blank' className='livelink'>Live</a>
                     </div>
                 </div>
             </div>
